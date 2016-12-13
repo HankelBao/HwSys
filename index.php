ec5ab4b5a6fe6a5ff338547e6cbaf76f7316cff1
HankelBao <hankelbao@outlook.com>
2016-12-13 12:41:33 +0800

    Change data succeed!!!

--- index.php
+++ index.php
@@ -13,7 +13,12 @@
                 print($x->due.": ".$x->description." </br>");
                 print($x->picture);
                 print("</br>");
+                $x->due = "1999/04/04";
             }
+            $save_xml = $HWData->asXML();
+            $file = fopen("test.xml","w");
+            fwrite($file,$save_xml);
+            fclose($file);
         ?>
     </body>
 </html>
