f015b040ddcce2b09aa0f3efe8dd614f6821812b
hankelbao <hankelbao@localhost.localdomain>
2017-06-06 11:45:14 +0800

    Update README and New Design

--- testbot.py
+++ testbot.py
@@ -1,9 +1,10 @@
-# coding=gbk
+#coding=gbk
 from qqbot import QQBotSlot as qqbotslot, RunBot
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
-    bot.SendTo(contact, contact.ctype)
+    if (contact.qq == "494541401"):
+        open('record','a').write(content)
 
 if __name__ == '__main__':
     RunBot()
