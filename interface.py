c04d787d87015d7d06ec23647a7a65fb525a7777
HankelBao <hankelbao@outlook.com>
2017-07-15 14:10:17 +0800

    finish the framework and a simple interface.py

--- interface.py
+++ interface.py
@@ -1,19 +1,13 @@
-# open("record","a").write("Hello World!\n")
 from qqbot import QQBotSlot as qqbotslot, RunBot
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
     if (contact.qq == "494541401"):
-
        open("record","a").write(content)
        open("record","a").write(member.qq)
        open("record","a").write(contact.nick)
        open("record","a").write(contact.name)
        open("record","a").write("\n")
 
-if __name__ == '__main__':
+def init():
     RunBot()
-
-
-# def GroupOnMessage(MessageSender, MessageContent):
-# def BuddyOnMessage(MessageSender, MessageContent):
---
+++ main.py
@@ -0,0 +1,5 @@
+import interface
+import interface
+
+if __name__ == '__main__':
+    interface.init()
