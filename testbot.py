1227ccf9b2c2d869684884e2be80dd81274acb30
HankelBao <hankelbao@outlook.com>
2017-07-15 13:48:31 +0800

    basic test finished, README for new target

--- testbot.py
+++ testbot.py
@@ -1,10 +1,19 @@
-#coding=gbk
+# open("record","a").write("Hello World!\n")
 from qqbot import QQBotSlot as qqbotslot, RunBot
 
 @qqbotslot
 def onQQMessage(bot, contact, member, content):
     if (contact.qq == "494541401"):
-        open('record','a').write(content)
+
+       open("record","a").write(content)
+       open("record","a").write(member.qq)
+       open("record","a").write(contact.nick)
+       open("record","a").write(contact.name)
+       open("record","a").write("\n")
 
 if __name__ == '__main__':
     RunBot()
+
+
+# def GroupOnMessage(MessageSender, MessageContent):
+# def BuddyOnMessage(MessageSender, MessageContent):
