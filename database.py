02b28161dc6b105c54fff870fb503514543e82bf
HankelBao <hankelbao@outlook.com>
2017-08-06 10:56:49 -0400

    really, vscode seems easier although not that beautiful as atom

--- database.py
+++ database.py
@@ -1,27 +1,21 @@
 import sqlite3
-
 def GetConnection():
     con = sqlite3.connect("hw.db")
     con.row_factory = sqlite3.Row
     return con
-
 def GetCursor():
     con = GetConnection()
     cu = con.cursor()
     return cu
-
 def GetTeacherQQ():
     cu = GetCursor()
     cu.execute("select * from rel_teacher")
-
     teacherQQ = []
     for item in cu.fetchall():
         teacherQQ.append(item['QQNumber'])
     return teacherQQ
-
 def AddRecord(TeacherId, SendTime, DueTime, Content):
     cu = GetCursor()
     cu.execute("insert into record values (" +TeacherId+ "," +SendTime+ "," +DueTime+ "," +Content+ ")")
-
 def init():
     return
